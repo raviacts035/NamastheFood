4dc9c5010ff9d5533ea32cc113873c4991ead0b9
raviacts035 <ravindrainturi035@gmail.com>
2023-05-14 15:57:54 +0530

    CSS Update: ItemCard, Code Update: Collapsible ItemGroup

--- components/ItemCard.js
+++ components/ItemCard.js
@@ -1,13 +1,13 @@
 const ItemCard=({name,price,id,imageId,description})=>{
     return (
-        <div className="flex justify-between" key={id}>
-            <div>
-                <h2>{name}</h2>
-                <p>{price/100} rs</p>
-                <p>{description?description:""}</p>
+        <div className="p-2 flex justify-between" key={id}>
+            <div className="gap-2">
+                <h2 className="font-bold text-[13px]">{name}</h2>
+                <p className="text-[13px] text-slate-950">₹ {price/100}</p>
+                <p className="py-2 text-[10px] text-slate-400">{description?description:<></>}</p>
             </div>
-            <div className="h-[100px] border-2 border-black">
-                {imageId?<img alt="Item Image" className="h-full" src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/"+imageId} />:''}
+            <div className="h-fit border-1 rounded-lg border-black">
+                {imageId?<img alt="Item Image" className="h-[100px] w-[100px] rounded-lg" src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/"+imageId} />:''}
             </div>
         </div>
     )
--- components/ItemGroup.js
+++ components/ItemGroup.js
@@ -1,16 +1,31 @@
-import ItemCard from "./Itemcard"
+import { useState } from "react"
+import ItemCard from "./ItemCard"
 
 const ItemGroup=(par)=>{
     // console.log(par.par)
+    const [isVisible,setIsVisible]=useState(true)
+
+    function setVisibility(){
+        setIsVisible(!isVisible)
+    }
     return (
-        <section key={par.par.title}>
-            <h3><b>{par.par.title}</b></h3>
-            {par.par.itemCards.map((r)=>{
-            return <>
-            <ItemCard {...r?.card?.info}/>
-            <hr className="my-2 border-1 w-full"/>
-            </>
-        })}
+        <section className="py-2" key={par.par.title}>
+            <div className="flex justify-between">
+                <span className="font-bold">{par.par.title} </span>
+                <button onClick={()=>{
+                    setVisibility()
+                }}>
+                    <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
+                </button>
+            </div>
+            {isVisible?<div id="dropdown">
+                {par.par.itemCards.map((r)=>{
+                return <>
+                    <hr className="my-2 border-1 w-full"/>
+                    <ItemCard {...r?.card?.info}/>
+                </>
+                })}
+            </div>:<></>}
         </section>
     )
 }
